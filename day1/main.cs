1ef3bf513c96d1d156256683f93bd12ffdbbb356
laurentgrangeau <laurent.grangeau@gmail.com>
2021-12-02 16:35:34 +0000

    WIP: Solve part 1

--- day1/main.cs
+++ day1/main.cs
@@ -1,12 +1,23 @@
 using System;
 
-namespace aoc-day-1 {
+namespace aocday1 {
     class main {
         static void main(string[] args) {
             string[] input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "puzzleInput.txt"));
             int[] report = (Array.ConvertAll(input, s => Int32.Parse(s)));
 
-            Console.WriteLine
+            Console.WriteLine();
+        }
+
+        private static int SolvePart1(int[] report) {
+            int result = 0;
+
+            for (int i = 1 ; i < report.Length; i++) {
+                if (report[i] > report[i - 1]) {
+                    result++;
+                }
+            }
+            return result;
         }
     }
 }
