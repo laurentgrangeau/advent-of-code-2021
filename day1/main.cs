ad3919b47353761c07c8f3eabf58965cb73461d7
laurentgrangeau <laurent.grangeau@gmail.com>
2021-12-02 16:29:13 +0000

    WIP: main program

---
+++ day1/main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace aoc-day-1 {
+    class main {
+        static void main(string[] args) {
+            string[] input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "puzzleInput.txt"));
+            int[] report = (Array.ConvertAll(input, s => Int32.Parse(s)));
+
+            Console.WriteLine
+        }
+    }
+}
