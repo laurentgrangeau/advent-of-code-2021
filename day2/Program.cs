1ab700e3051f53959f9d6fa53e4f543c923e5d96
laurentgrangeau <laurent.grangeau@gmail.com>
2021-12-03 09:59:51 +0000

    Day 2 challenge

---
+++ day2/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace aocday1 {
+    class Program {
+        static void Main(string[] args) {
+            string[] input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt"));
+
+            Console.WriteLine(SolvePart1(input));
+        }
+
+        private static int SolvePart1(string[] input) {
+            int result = 0;
+            int position = 0;
+            int depth = 0;
+
+            for (int i = 0; i < input.Length; i++) {
+                string[] words = input[i].Split(' ');
+                switch (words[0])
+                {
+                    case "forward":
+                        position += int.Parse(words[1]);
+                        break;
+                    case "down":
+                        depth += int.Parse(words[1]);
+                        break;
+                    case "up":
+                        depth -= int.Parse(words[1]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            result = position * depth;
+
+            return result;
+        }
+    }
+}
